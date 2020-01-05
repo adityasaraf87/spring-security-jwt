7f4d6f71aad16535eef94468596510da0e026841
aditya.saraf <adityasaraf87@gmail.com>
2020-01-05 11:36:05 +0530

    Commit Spring Security JWT

---
+++ src/main/java/com/security/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.security.springsecurityjwt.models;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 2650998868922929476L;
+	private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
